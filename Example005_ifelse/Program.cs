9b9b9f9743f7321a0c7877dd8d02944ede552bb9
wakhsavior <wakhsavior@gmail.com>
2022-06-30 00:24:58 +0300

    Add example 005 to lecture

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "денис"){
+    Console.WriteLine("Ehhoy, Denis! ");
+}
+else {
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+    
+}
+
+

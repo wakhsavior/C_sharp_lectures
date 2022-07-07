9c447f293a19e55eef021ed7047e0878031201af
wakhsavior <wakhsavior@gmail.com>
2022-07-07 19:22:02 +0300

    Add examples from Lecture 2

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// int Max(int[] arg){
+//     int result = arg[0];
+//     int length = Array.MaxLength;    
+//     while if (arg2 > result)  result = arg2;
+//     if (arg3 > result)  result = arg3;
+//     return result;
+// }
+
+int[] array = {1,2324,34,47,58,64,337,8223,95};
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1124,322,433,324,48,512,461,72,48};
+
+int n = array.Length;
+int find = 48;
+
+int index = 0;
+
+while(index < n){
+
+    if(array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+    index++;
+}

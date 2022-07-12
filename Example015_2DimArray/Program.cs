99738bb24defd7d516ac49740cfa0dd83d7761c4
wakhsavior <wakhsavior@gmail.com>
2022-07-12 22:36:24 +0300

    Add Lecture 4 2D_array and Recursion

---
+++ Example015_2DimArray/Program.cs
@@ -0,0 +1,41 @@
+﻿// string[,] table = new string[2,5];  //2 строчки и 5 столбцов
+// table[1,2] = "слово";  // индексы меняются от 0
+//                     // table[0,0] table[0,2]
+//                     // table[1,0] table[1,1]
+
+// for (int rows = 0;rows<2;rows++)
+// {
+//     for(int columns = 0; columns<5;columns++)
+//     {
+//         Console.WriteLine($"-{table[rows,columns]}-");
+//     }
+// }
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
---
+++ Example018_Fibonnachi_recurs/Program.cs
@@ -0,0 +1,11 @@
+﻿int Fibonacci(int n)
+{
+    if(n==1 || n==2) return 1;
+    else return Fibonacci(n-1) +Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    
+    Console.WriteLine($"fib({i}) - {Fibonacci(i)}");   
+}
